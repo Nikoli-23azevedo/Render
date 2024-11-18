cc92c6d4d3d345df15410db03eb73bd76b336017
Nikoli-23azevedo <nikoli.azevedo@icloud.com>
2024-11-18 09:34:03 -0300

    Update server.js

--- server.js
+++ server.js
@@ -19,7 +19,7 @@ app.use(cors({
     }
 }));
 
-const uri = "mongodb+srv://nicolassiribolabordini:OzV8YeG6z813dPWs@cluster0.btxom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+const uri = "mongodb+srv://Nikoli:fcPOG8wIZeNQqfhZ@cluster0.btxom.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
 const client = new MongoClient(uri, {
